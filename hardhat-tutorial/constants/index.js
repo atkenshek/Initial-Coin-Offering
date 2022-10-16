09140014d37f86e054d0f637b0ceae66f3d60226
Meiram Sopy Temirzhanov <random9804@yandex.ru>
2022-10-16 13:15:59 +0600

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xc0351Fb8af3edd05591cC39Bd6EAc3ed40E1A76b";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
